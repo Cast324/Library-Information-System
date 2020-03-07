e1a95b45d2649ac8b114b4f9d146f0005c5604d4
Michael Blades <michaelblades12@gmail.com>
2020-03-06 22:57:06 -0600

    Updated routes and added bootstrap to nodejs

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,27 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
+});
+
+router.get('/landing', function(req, res, next) {
+  res.render('landing');
+});
+
+router.get('/administration', function(req, res, next) {
+  res.render('administration');
+});
+
+router.get('/circulation', function(req, res, next) {
+  res.render('circulation');
+});
+
+router.get('/reference', function(req, res, next) {
+  res.render('reference');
+});
+
+router.get('/itportal', function(req, res, next) {
+  res.render('itportal');
 });
 
 module.exports = router;
