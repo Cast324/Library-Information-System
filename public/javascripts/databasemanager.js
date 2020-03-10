50400270a75d1c308823efa5742749dea0e5b55f
Michael Blades <michaelblades12@gmail.com>
2020-03-09 21:20:53 -0500

    Updated Administration page to use database for patrons

--- public/javascripts/databasemanager.js
+++ public/javascripts/databasemanager.js
@@ -3,18 +3,17 @@ const dotenv = require('dotenv')
 const mysql = require('mysql')
 dotenv.config()
 
-var connection = mysql.createConnection({
-    host     : process.env.DBHOST,
-    user     : process.env.DBUSER,
-    password : process.env.DBPASSWORD,
-    database : process.env.DBDATABASE
-  })
-   
-  connection.connect()
-   
-  connection.query('SHOW TABLES', function (error, results, fields) {
-    if (error) throw error
-    console.log('The solution is: ', results)
-  })
+var mysqlConnection = mysql.createConnection({
+  host     : process.env.DBHOST,
+  user     : process.env.DBUSER,
+  password : process.env.DBPASSWORD,
+  database : process.env.DBDATABASE
+});
 
-  connection.end()
+mysqlConnection.connect((err) => {
+  if (err) throw err;
+  console.log("Database Connected")
+})
+
+
+  module.exports = mysqlConnection
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var databasemanager = require('../public/javascripts/databasemanager.js')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -11,7 +12,13 @@ router.get('/landing', function(req, res, next) {
 });
 
 router.get('/administration', function(req, res, next) {
-  res.render('administration');
+  databasemanager.query("SELECT * FROM Patron", (err, rows, fields) => {
+    if (err) throw err;
+    const patrons = {
+      patron: rows
+    }
+    res.render('administration', patrons)
+  })
 });
 
 router.get('/circulation', function(req, res, next) {
