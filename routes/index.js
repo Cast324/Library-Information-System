467a64cb636caf7d0e8b9b9ff856d71cbb2480d8
Dulio Gonzalez <dulio.gonzalez@gmail.com>
2020-04-05 22:15:28 -0500

    Add Accounts to Administration View

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
-var databasemanager = require('../public/javascripts/databasemanager.js')
+var databasemanager = require('../public/javascripts/databasemanager.js');
+var bodyParser = require('body-parser');
+var urlencodedParser = bodyParser.urlencoded({ extended: false});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -21,6 +23,41 @@ router.get('/administration', function(req, res, next) {
   })
 });
 
+router.post('/administration/account', urlencodedParser, function(req, res) {
+  if (!req.body) return res.sendStatus(400)
+  let current_datetime = new Date()
+  var accountID = 0;
+  let dbDate = current_datetime.getFullYear() + "-" + (current_datetime.getMonth() + 1) + "-" + current_datetime.getDate()
+  var sql = 'insert into Account (creationDate, state, password) values (\''+dbDate+'\',\'active\',\''+req.body.passwordInput+'\');'
+  databasemanager.query(sql, function(err, result) {
+    if (err) throw err;
+    console.log("account record made, ID: " + result.insertId);
+    accountID = result.insertId;
+    console.log(accountID);
+  })
+  console.log(accountID);
+  if (req.body.selectAccount == 'patronAccount') {
+    let patronSql = 'insert into Patron (accountID, firstName, lastName, address, email) values ('+accountID+',\''+req.body.firstNameInput+'\',\''+req.body.lastNameInput+'\',\''+req.body.addressInput+'\',\''+req.body.emailInput+'\');';
+    databasemanager.query(patronSql, function(err, result) {
+      if (err) throw err;
+      console.log("Patron account made, ID:" + result.insertId);
+    })
+  } else if (req.body.selectAccount == 'staffAccount') {
+    let patronSql = 'insert into Staff (accountID, firstName, lastName, email, isAdmin) values ('+accountID+',\''+req.body.firstNameInput+'\',\''+req.body.lastNameInput+'\',\''+req.body.emailInput+'\','+req.body.isAdminRadio+');';
+    databasemanager.query(patronSql, function(err, result) {
+      if (err) throw err;
+      console.log("Patron account made, ID:" + result.insertId);
+    })
+  }  
+
+  
+  console.log(req.body);
+  res.redirect('/administration');
+});
+
+function changeID(id) {
+
+}
 router.get('/circulation', function(req, res, next) {
   res.render('circulation');
 });
